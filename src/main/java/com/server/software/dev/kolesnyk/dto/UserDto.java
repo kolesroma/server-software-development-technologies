541029301ac0c689c8b1c6ac74ff98e46b6a27e0
Roman Kolesnyk <romakoles1@ukr.net>
2022-11-07 21:28:26 +0200

    change accounting on update/create record

--- src/main/java/com/server/software/dev/kolesnyk/dto/UserDto.java
+++ src/main/java/com/server/software/dev/kolesnyk/dto/UserDto.java
@@ -1,10 +1,12 @@
 package com.server.software.dev.kolesnyk.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -16,9 +18,11 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
+@Builder
 public class UserDto implements Serializable {
     @NotBlank
     private String name;
     @NotNull
+    @Min(value = 0, message = "cannot apply balance < 0")
     private Integer balance;
 }
--- src/main/java/com/server/software/dev/kolesnyk/service/impl/RecordServiceImpl.java
+++ src/main/java/com/server/software/dev/kolesnyk/service/impl/RecordServiceImpl.java
@@ -1,16 +1,21 @@
 package com.server.software.dev.kolesnyk.service.impl;
 
 import com.server.software.dev.kolesnyk.dto.RecordDto;
+import com.server.software.dev.kolesnyk.dto.UserDto;
 import com.server.software.dev.kolesnyk.entity.RecordEntity;
+import com.server.software.dev.kolesnyk.entity.UserEntity;
 import com.server.software.dev.kolesnyk.exception.EntityNotFound;
 import com.server.software.dev.kolesnyk.repository.RecordRepository;
 import com.server.software.dev.kolesnyk.service.CategoryService;
 import com.server.software.dev.kolesnyk.service.RecordService;
 import com.server.software.dev.kolesnyk.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.ValidationException;
+import javax.validation.Validator;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Optional;
@@ -20,23 +25,35 @@ public class RecordServiceImpl implements RecordService {
     private final RecordRepository recordRepository;
     private final UserService userService;
     private final CategoryService categoryService;
+    private final Validator validator;
 
-    public RecordServiceImpl(RecordRepository recordRepository, UserService userService, CategoryService categoryService) {
+    public RecordServiceImpl(RecordRepository recordRepository, UserService userService, CategoryService categoryService, Validator validator) {
         this.recordRepository = recordRepository;
         this.userService = userService;
         this.categoryService = categoryService;
+        this.validator = validator;
     }
 
     @Override
     @Transactional
     public void createRecord(RecordDto recordDto) {
+        UserEntity user = userService.getUser(recordDto.getUserId())
+                .orElseThrow(EntityNotFound::new);
         RecordEntity record = RecordEntity.builder()
-                .user(userService.getUser(recordDto.getUserId()).orElseThrow(EntityNotFound::new))
+                .user(user)
                 .category(categoryService.getCategory(recordDto.getCategoryId()).orElseThrow(EntityNotFound::new))
                 .createdAt(Optional.ofNullable(recordDto.getCreatedAt()).orElse(LocalDateTime.now()))
                 .outgo(recordDto.getOutgo())
                 .build();
+        UserDto userDto = UserDto.builder()
+                .name(user.getName())
+                .balance(user.getAccounting().getBalance() - record.getOutgo())
+                .build();
+        if (validator.validate(userDto).size() > 0) {
+            throw new ValidationException("cannot apply balance < 0 or empty name");
+        }
         recordRepository.save(record);
+        userService.updateUser(userDto, user.getId());
     }
 
     @Override
@@ -50,17 +67,26 @@ public class RecordServiceImpl implements RecordService {
                 .getContent();
     }
 
-    //    todo calculate user balance when changing and creating
     @Override
     @Transactional
     public void updateRecord(RecordDto recordDto, Integer id) {
         RecordEntity record = recordRepository.findById(id)
                 .orElseThrow(EntityNotFound::new);
-        record.setUser(userService.getUser(recordDto.getUserId()).orElseThrow(EntityNotFound::new));
+        UserEntity user = userService.getUser(recordDto.getUserId())
+                .orElseThrow(EntityNotFound::new);
+        UserDto userDto = UserDto.builder()
+                .name(user.getName())
+                .balance(user.getAccounting().getBalance() + record.getOutgo() - recordDto.getOutgo())
+                .build();
+        record.setUser(user);
         record.setCategory(categoryService.getCategory(recordDto.getCategoryId()).orElseThrow(EntityNotFound::new));
         record.setCreatedAt(Optional.ofNullable(recordDto.getCreatedAt()).orElse(LocalDateTime.now()));
         record.setOutgo(recordDto.getOutgo());
+        if (validator.validate(userDto).size() > 0) {
+            throw new ValidationException("cannot apply balance < 0 or empty name");
+        }
         recordRepository.save(record);
+        userService.updateUser(userDto, user.getId());
     }
 
     @Override
